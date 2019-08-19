807c54546819fb7e71dfddbaed99da2d0f0d946a
Matheus <matheuscruzhen@gmail.com>
2019-08-18 23:09:58 -0300

    added dropdown menu

--- resources/scripts.js
+++ resources/scripts.js
@@ -44,6 +44,23 @@ $(document).ready(function() {
         window.location.hash = hash;
       });
     } // End if
-  });   
+  });
+  
+  // STICKY NAV
+
+  $('.js--nav-icon').click(function() {
+    var nav = $('.js--main-nav');
+    var icon = $('.js--nav-icon i');
+
+    nav.slideToggle(200);
+    if (icon.hasClass('ion-navicon-round')) {
+      icon.addClass('ion-close-round');
+      icon.removeClass('ion-navicon-round');
+    } else {
+      icon.addClass('ion-navicon-round');
+      icon.removeClass('ion-close-round');
+    }
+
+  });
 
 });
